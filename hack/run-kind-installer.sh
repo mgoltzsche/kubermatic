49f6a40f8ff3023fc22d7eee3950f9fa0be08f22
Max Goltzsche <max.goltzsche@kubermatic.com>
2026-03-02 11:23:05 +0100

    chore: try to pin tags in kind installer script
    Not sufficient since more images need to be pinned.
    Also, it turns out that after I created a new git commit many other images cannot be run that were previously pulled from a remote registry apparently.
    
    Signed-off-by: Max Goltzsche <max.goltzsche@kubermatic.com>

--- hack/run-kind-installer.sh
+++ hack/run-kind-installer.sh
@@ -1,35 +1,39 @@
 #!/usr/bin/env bash
 
-set -eux
+#export IMAGE_TAG="$(date +%s)"
+export IMAGE_TAG="$(git describe --tags --always --match='v*')"
+GIT_COMMIT_SHA="$(git rev-parse HEAD)"
+
+set -eu
 
 # Copy CRDs into kubermatic-operator chart directory
-source hack/lib.sh
-copy_crds_to_chart
-set_crds_version_annotation
+(echo Copying CRDs...; source hack/lib.sh && copy_crds_to_chart && set_crds_version_annotation)
+
+set -x
 
 # Build the installer
 go build -o kubermatic-installer ./cmd/kubermatic-installer
 
 # Build the kubermatic-operator container image
-make docker-build
+make docker-build TAGS="$IMAGE_TAG $GIT_COMMIT_SHA"
 
 # Build the dashboard (and API) container image
 # (This assumes the dashboard repo is checked out at ../dashboard)
-make -C ../dashboard docker-build IMAGE_TAG=NA KUBERMATIC_EDITION=ce
+make -C ../dashboard docker-build IMAGE_TAG=$IMAGE_TAG KUBERMATIC_EDITION=ce
 
 # Modify the image tag within the kubermatic-operator chart values
 # (This is to use the locally built image)
-yq eval -i '.kubermaticOperator.image.tag="latestbuild"' ./charts/kubermatic-operator/values.yaml
+yq eval -i '.kubermaticOperator.image.tag=strenv(IMAGE_TAG)' ./charts/kubermatic-operator/values.yaml
 
 # TODO: Prepare the examples directory - copy from last release? where to get the latest examples from?
 
 # Set the tag of the locally built dashboard API container image
-yq eval -i '.spec.api.dockerTag="NA"' ./examples/kubermatic.example.yaml
+yq eval -i '.spec.api.dockerTag=strenv(IMAGE_TAG)' ./examples/kubermatic.example.yaml
 
 (
 	# Load locally built container images into kind once it is available
 	sleep 20 # wait for kind container to initialize
-	kind load docker-image quay.io/kubermatic/kubermatic:latestbuild quay.io/kubermatic/dashboard:NA --name kkp-cluster
+	kind load docker-image quay.io/kubermatic/kubermatic:$IMAGE_TAG quay.io/kubermatic/kubermatic:$GIT_COMMIT_SHA quay.io/kubermatic/dashboard:$IMAGE_TAG --name kkp-cluster
 ) &
 
 # Run the installer
